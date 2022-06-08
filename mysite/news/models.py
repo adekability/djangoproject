cd66aeaa6412530f71b5f97ec0847a8c1eda3495
Adil <43477533+adekability@users.noreply.github.com>
2022-06-08 15:13:23 +0600

    refactor: admin panel

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = "Новости"
+        ordering = ['-created_at', '-title']
+
+    def __str__(self):
+        return self.title
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,15 @@
+from django.shortcuts import render
 from django.http.response import HttpResponse
+from .models import News
 
 
 def index(request):
-    return HttpResponse("Hello, world")
+    news = News.objects.all()
+    context = {'news': news,
+               'title': 'Список новостей'}
+    return render(request=request,
+                  template_name='news/index.html',
+                  context=context)
 
 
 def test(request):
